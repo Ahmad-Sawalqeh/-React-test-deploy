571c831e7c41a5a51afad8f14b390a4601beb512
Ahmad-Sawalqeh <runner@fv-az12.dj0mtgxin0yulngbg20r4iesxh.cx.internal.cloudapp.net>
2020-02-24 14:39:56 +0000

    Updates

---
+++ precache-manifest.04f4f2da7e072e5226a8bce5ea184244.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ac2619e53d36828521e44436d06b1d",
+    "url": "/react-test-deploy/index.html"
+  },
+  {
+    "revision": "7b8db39071ef210aaa34",
+    "url": "/react-test-deploy/static/js/2.b0715784.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-test-deploy/static/js/2.b0715784.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c31be2c913c38828283",
+    "url": "/react-test-deploy/static/js/main.6ab3c9fd.chunk.js"
+  },
+  {
+    "revision": "5ea67c736e515d485b09",
+    "url": "/react-test-deploy/static/js/runtime-main.de80936f.js"
+  }
+]);
